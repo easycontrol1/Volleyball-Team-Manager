180173dad88bc676b5c8c5fdc8e0befd30dba704
Simeon <Simeon@DESKTOP-C6GBBS7>
2026-03-09 21:09:47 +0200

    Add project files.

---
+++ Volleyball Team Manager/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Volleyball_Team_Manager.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
+    {
+    }
+}
